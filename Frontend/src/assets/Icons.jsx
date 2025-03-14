5efe32a54e337ef8f11ed813e8c33bd4895940a9
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-14 23:02:42 +0530

    Icons_setup

---
+++ Frontend/src/assets/Icons.jsx
@@ -0,0 +1,19 @@
+import ProfileSVG from "./profile.svg";
+import PostSVG from "./create-post.svg";
+import FeedSVG from "./home.svg";
+import SearchSVG from "./search-5-svgrepo-com.svg";
+import Register from "./register.svg";
+import Login from "./LoginSVG.svg";
+import LogOut from "./shutdown-small-svgrepo-com.svg"
+
+const Icons = {
+    Profile: ProfileSVG,
+    Post: PostSVG,
+    Feed: FeedSVG,
+    Search: SearchSVG,
+    Register: Register,
+    Login: Login,
+    Logout: LogOut
+};
+
+export default Icons;
