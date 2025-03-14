8fac1a1434a725875cc202e3c918c876147d2cfb
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-14 23:00:34 +0530

    app_setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import AppRoutes from "./routes/Routes"
+
+const App = () => {
+
+  return (
+    <div className="h-full w-full ">
+      <AppRoutes />
+
+
+    </div>
+  )
+}
+
+export default App
